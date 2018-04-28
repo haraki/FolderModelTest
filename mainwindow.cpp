66d36c805109d10183b6406b06ec87d6007b8035
Masashi Haraki <masa.haraki@gmail.com>
2018-04-28 22:45:42 +0900

    1st commit.

---
+++ mainwindow.cpp
@@ -0,0 +1,75 @@
+﻿#include <QFileSystemModel>
+#include <QSortFilterProxyModel>
+#include <QDir>
+#include <QDebug>
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    fileSystemModel_ = new QFileSystemModel(this);
+    fileSystemModel_->setRootPath(QDir::homePath());
+    fileSystemModel_->setFilter(QDir::NoDot | QDir::AllDirs | QDir::Files);
+
+    sortFilterProxy_ = new QSortFilterProxyModel();
+    sortFilterProxy_->setSourceModel(fileSystemModel_);
+    sortFilterProxy_->setDynamicSortFilter(true);
+    sortFilterProxy_->setSortCaseSensitivity(Qt::CaseInsensitive);
+    sortFilterProxy_->setFilterKeyColumn(0);
+
+    ui->tableView->setModel(sortFilterProxy_);
+    ui->tableView->setRootIndex(sortFilterProxy_->mapFromSource(fileSystemModel_->index(fileSystemModel_->rootPath())));
+
+    ui->tableView->horizontalHeader()->setSortIndicator(0, Qt::AscendingOrder);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_tableView_doubleClicked(const QModelIndex &index)
+{
+    QModelIndex i = sortFilterProxy_->mapToSource(index);
+    if(!i.isValid())
+    {
+        qDebug() << "invalid.";
+
+        return;
+    }
+
+    if(!fileSystemModel_->isDir(i))
+    {
+        qDebug() << "not directory.";
+
+        return;
+    }
+
+    QString path = fileSystemModel_->filePath(i);
+    qDebug() << "dir : " << path;
+
+    ui->tableView->setRootIndex(sortFilterProxy_->mapFromSource(fileSystemModel_->setRootPath(path)));
+}
+
+void MainWindow::on_caseSensitiveCheckBox_stateChanged(int arg1)
+{
+    if(ui->caseSensitiveCheckBox->isChecked())
+    {
+        qDebug() << "Case sensitive.";
+        sortFilterProxy_->setSortCaseSensitivity(Qt::CaseSensitive);
+    }
+    else
+    {
+        qDebug() << "Case insensitive.";
+        sortFilterProxy_->setSortCaseSensitivity(Qt::CaseInsensitive);
+    }
+
+    if(ui->tableView->horizontalHeader()->sortIndicatorSection() == 0)
+    {
+        sortFilterProxy_->sort(0, ui->tableView->horizontalHeader()->sortIndicatorOrder());
+    }
+}
---
+++ mainwindow.h
@@ -0,0 +1,33 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class QFileSystemModel;
+class QSortFilterProxyModel;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_tableView_doubleClicked(const QModelIndex &index);
+
+    void on_caseSensitiveCheckBox_stateChanged(int arg1);
+
+private:
+    Ui::MainWindow *ui;
+
+    QFileSystemModel* fileSystemModel_ = Q_NULLPTR;
+    QSortFilterProxyModel* sortFilterProxy_ = Q_NULLPTR;
+};
+
+#endif // MAINWINDOW_H
